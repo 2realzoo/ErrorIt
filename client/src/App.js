62b02b334cfb6e1cb93b191f61de3650fa961998
2realzoo <lunalee00@gmail.com>
2023-02-17 13:40:30 +0900

    [FE][CHORE]:React 작업환경 빌드

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Question from "./pages/Question";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/question" element={<Question />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
