371f221fdd0fc81997cf8ff40b6ebeadd54ea6e7
2realzoo <lunalee00@gmail.com>
2023-02-21 11:12:18 +0900

    [FE][REFACTOR]:Header 내부 MenuSideBar 컴포넌트화

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from "react";
 import styled, { css } from "styled-components";
 import "./Header.css";
 import { TbSearch } from "react-icons/tb";
-import { FaGlobeAsia } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
-import { currentPage } from "../../reducers/actions";
 import { useNavigate } from "react-router-dom";
+import MenuSideBar from "./MenuSideBar";
 
 const HeaderContainer = styled.header`
   position: fixed;
@@ -29,71 +28,6 @@ const HeaderWrapper = styled.div`
   width: 97.2307692rem;
   max-width: 100%;
   align-items: center;
-`;
-const MenuWrapper = styled.a`
-  display: flex;
-  height: 100%;
-  padding: 0 var(--su16);
-  flex-shrink: 0;
-  align-items: center;
-  justify-content: center;
-  &:hover {
-    background-color: var(--theme-topbar-item-background-hover);
-  }
-`;
-const MenuSidebar = styled.div`
-  display: ${(props) => props.display || "block"};
-  position: fixed;
-  top: 50px;
-  height: fit-content;
-  box-shadow: 0 0 0 hsl(210deg 8% 5% / 5%);
-  transition: box-shadow ease-in-out 0.1s, transform ease-in-out 0.1s;
-  transform: translateZ(0);
-  background-color: var(--white);
-`;
-const SidebarNav = styled.ol`
-  padding-top: 24px;
-  margin: 0 0 var(--su12);
-`;
-const SelectedItem = styled.li`
-  position: relative;
-  width: 240px;
-  display: flex;
-  cursor: pointer;
-  text-transform: ${(props) => props.textTransform || "none"};
-`;
-const NavItemWrapper = styled.li`
-  position: relative;
-  width: 240px;
-  display: flex;
-  cursor: pointer;
-`;
-const NavItem = styled.a`
-  display: flex;
-  align-items: center;
-  padding: 10px 4px 10px 18px;
-  color: var(--black-600);
-  line-height: var(--lh-md);
-  font-size: var(--fs-body1);
-  &:hover {
-    color: var(--black-900);
-  }
-`;
-const Itemflex = styled.span`
-  padding-right: 5px;
-`;
-const NavLink = styled.a`
-  display: block;
-  font-weight: bold;
-  color: var(--black-900);
-  background: var(--black-050);
-  border-right: 3px solid var(--theme-primary-color);
-  width: 100%;
-  padding: 10px 4px 10px 18px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  font-size: var(--fs-body1);
 `;
 const Logo = styled.a`
   margin-right: 8px;
@@ -176,8 +110,7 @@ const SearchInput = styled.input`
 
 function Header() {
   const [isLogin, setIsLogin] = useState(false);
-  const [openMenu, setOpenMenu] = useState(false);
-  const { currentPageReducer } = useSelector((state) => state);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -186,12 +119,6 @@ function Header() {
   // }, [currentPageReducer]);
   // currentPageReducer 상태 확인용 코드
 
-  const handleClickMenu = () => {
-    setOpenMenu(!openMenu);
-  };
-  const handlePageHome = () => {
-    dispatch(currentPage("Home"));
-  };
   const handleLogout = () => {
     dispatch(isLogin(false));
     navigate("/");
@@ -199,57 +126,7 @@ function Header() {
   return (
     <HeaderContainer>
       <HeaderWrapper>
-        <MenuWrapper onClick={handleClickMenu}>
-          <span className={`menu-icon ${openMenu ? "active" : ""}`}></span>
-        </MenuWrapper>
-        {openMenu ? (
-          <MenuSidebar>
-            <SidebarNav>
-              {currentPageReducer === "Home" ? (
-                <SelectedItem onClick={handlePageHome}>
-                  <NavLink>Home</NavLink>
-                </SelectedItem>
-              ) : (
-                <NavItemWrapper onClick={handlePageHome}>
-                  <NavItem href="/">Home</NavItem>
-                </NavItemWrapper>
-              )}
-              {currentPageReducer === "Question" ? (
-                <SelectedItem>
-                  <NavLink>Questions</NavLink>
-                  <FaGlobeAsia />
-                </SelectedItem>
-              ) : (
-                <NavItemWrapper>
-                  <NavItem>
-                    <Itemflex>Questions</Itemflex>
-                    <FaGlobeAsia />
-                  </NavItem>
-                </NavItemWrapper>
-              )}
-              {currentPageReducer === "Tags" ? (
-                <SelectedItem>
-                  <NavLink>Tags</NavLink>
-                </SelectedItem>
-              ) : (
-                <NavItemWrapper>
-                  <NavItem>Tags</NavItem>
-                </NavItemWrapper>
-              )}
-              {currentPageReducer === "Users" ? (
-                <SelectedItem>
-                  <NavLink>Users</NavLink>
-                </SelectedItem>
-              ) : (
-                <NavItemWrapper>
-                  <NavItem>Users</NavItem>
-                </NavItemWrapper>
-              )}
-            </SidebarNav>
-          </MenuSidebar>
-        ) : (
-          <MenuSidebar display="none"></MenuSidebar>
-        )}
+        <MenuSideBar />
         <Logo href="/">
           <Img
             className="logo-img"
