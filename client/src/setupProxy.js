255939720702d1a2796143af2b4de10f4c53408e
2realzoo <lunalee00@gmail.com>
2023-03-24 14:09:28 +0900

    [FE][CHORE] 프록시 설정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://13.209.10.249:8080/",
+      target: "https://5e6f-220-95-3-215.jp.ngrok.io/",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
